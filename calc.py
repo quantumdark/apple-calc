6b27689a3fecf6e30a4fe0b58c600e64fb214245
Yegor Dolgopolov <quantumdark@gmail.com>
2017-11-20 13:26:24 -0800

    Support of multidigit figures added

--- calc.py
+++ calc.py
@@ -10,7 +10,6 @@ multiplication/division/parenthesis or any other operator), input is
 well-formed with alternating operand/operator so no error checking is needed,
 please do not use any built-in calculator function.
 """
-import argparse
 import string
 
 DIGITS = string.digits
@@ -20,24 +19,49 @@ OPERATORS = {
 }
 
 
-def calc(expr):
-    accum = 0
-    operator = OPERATORS['+']
+def tokenize(expr):
+    # generator
+    operand = ''
     for ch in expr:
         if ch in DIGITS:
-            operand = ord(ch) - ord('0')  # char to int
-            accum = operator(accum, operand)
+            operand += ch
         elif ch in OPERATORS:
+            if operand:
+                yield int(operand)
+                operand = ''
             operator = OPERATORS[ch]
+            yield operator
         else:
             raise SyntaxError('unknown character')
+    if operand:
+        yield int(operand)
+
+
+def calc(expr):
+    accum = 0
+    operand = None
+    operator = OPERATORS['+']
+    for token in tokenize(expr):
+        if callable(token):
+            operator = token
+        else:
+            operand = token
+            accum = operator(accum, operand)
     return accum
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('expression')
-    # args = parser.parse_args()
-    # result = calc(args.expression)
-    result = calc('-1-1-1-2-3-4')  # 12
-    print('result: %s' % result)
+    result = calc('-1-1-1-2-3-4')
+    assert result == -12
+
+    result = calc('120-1-1-2-3-4')
+    assert result == 109
+
+    result = calc('-1024')
+    assert result == -1024
+
+    result = calc('2048')
+    assert result == 2048
+
+    result = calc('2048+2')
+    assert result == 2050
